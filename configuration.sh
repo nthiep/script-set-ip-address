0700a4d3f205b9145b000ec44b4790e4eea8171b
nthiep <hiep.iuh@gmail.com>
2015-11-26 23:26:09 +0700

    add comment

--- configuration.sh
+++ configuration.sh
@@ -1,43 +1,91 @@
-# config file
-
-NETWORK_CONFIG_DEBIAN="interfaces"
-NETWORK_CONFIG_REDHAT="/etc/sysconfig/network-scripts/ifcfg-"
+#!/bin/bash
+##################################################
+# Name: configuration.sh
+# Description: File cau hinh ip va lay dia chi ip tren remote host
+#
+##################################################
+# Set Script Variables
+#
+# dia chi file cau hinh Debian hoac Ubuntu
+NETWORK_CONFIG_DEBIAN="/home/hiep/kichban/TieuLuanKB/interfaces"
+# dia chi file cau hinh tren redhat fedora centos
+#NETWORK_CONFIG_REDHAT="/etc/sysconfig/network-scripts/ifcfg-"
+NETWORK_CONFIG_REDHAT="/home/hiep/kichban/TieuLuanKB/ifcfg-"
+# dia chi file luu lai cac buoc cau hinh
 LOG_FILE="settingIpLog.txt"
-
+##################################################
+# Ham lay dia chi ip tu lenh ifconfig neu hien tai dang su dung ip dong
+#
 getIfconfigInfo(){
 	[ $# -eq 0 ] && return 1
 	ip=$(ifconfig $1  2> /dev/null|grep "inet addr"|cut -f 2 -d ":"|cut -f 1 -d " ")
 	netmask=$(ifconfig $1  2> /dev/null|grep "Mask"|cut -f 4 -d ":")
 	[ -n "$ip" -a -n "$netmask" ] && echo "$ip:$netmask" || echo ""
 }
+
+##################################################
+# Ham lay dia chi ip tu lenh ifconfig neu hien tai dang su dung ip dong
+# hoac tu file cau hinh tren may Redhat centos
+#
 getIpInfoRedhat(){
+	echo "config getIpInfoRedhat: $1" >> $LOG_FILE	
+	[ -f "$NETWORK_CONFIG_REDHAT$1" ] || return 1
 	[ $# -eq 0 ] && return 1
-	ip=$(sed -n 's/IPADDR=//p'  interfaces|sed -e 's/^[[:space:]]*//')
-	netmask=$(sed -n 's/NETMASK=//p'  interfaces|sed -e 's/^[[:space:]]*//')
+	ip=$(sed -n 's/IPADDR=//p'  $NETWORK_CONFIG_REDHAT$1|sed -e 's/^[[:space:]]*//')
+	netmask=$(sed -n 's/NETMASK=//p'  $NETWORK_CONFIG_REDHAT$1|sed -e 's/^[[:space:]]*//')
+
+	echo "config getIpInfoRedhat: ip:$ip" >> $LOG_FILE
+	echo "config getIpInfoRedhat: netmask:$netmask" >> $LOG_FILE
 	[ -n "$ip" -a -n "$netmask" ] && { echo "$ip:$netmask"; return 0; }
 	ipInfo=$(getIfconfigInfo $1)
+	echo "config getIpInfoRedhat: ipInfo:$ipInfo" >> $LOG_FILE
 	[ -n "$ipInfo" ] && echo $ipInfo || return 1
 }
 
+##################################################
+# Ham lay string cau hinh static hay dhcp cho may debian
+#
 getConfigIfStringDebian(){
 	[ "$2" == "dhcp" ] && printf "iface $1 inet dhcp"'\\n' || printf "iface $1 inet static"'\\n'
 	return 0
 }
+
+##################################################
+# Ham lay string cau hinh ip, netmask, gateway cho may debian
+#
 getConfigIpStringDebian(){
 	[ -n "$1" ] && printf '\\t'"address $1"'\\n'
 	[ -n "$2" ] && printf '\\t'"netmask $2"'\\n'
 	[ -n "$3" ] && printf '\\t'"gateway $3"'\\n'
 }
+
+##################################################
+# Ham cau hinh ip cho may debian
+#
 setIpDebian(){
+	# Kiem tra neu file cau hinh chua tao hoac khong co interface cau hinh
+	#
 	[ -f "$NETWORK_CONFIG_DEBIAN" ] && grep -q "iface $1" $NETWORK_CONFIG_DEBIAN || \
 	{ echo "iface $1" >> $NETWORK_CONFIG_DEBIAN; echo "config setIpDebian: Config file $1 not found" >> $LOG_FILE; }	
+	
+	# cau lenh sed xoa tat ca cac dong cau hinh trong interface
+	#
 	sed -i.bak "/iface $1/,/iface/{//!d}" $NETWORK_CONFIG_DEBIAN
+	
+	# cau lenh sed cau hinh dhcp
+	#
 	[ "$2" == "dhcp" ] && { sed -i "s/iface $1.*/$(getConfigIfStringDebian $1 $2)/" $NETWORK_CONFIG_DEBIAN; return 0; }
+	# ghi vao file log
 	echo "s/iface $1.*/$(getConfigIfStringDebian $1 $2)$(getConfigIpStringDebian $3 $4 $5)/" >> $LOG_FILE 
+	# cau lenh sed cau hinh static
 	sed -i "s/iface $1.*/$(getConfigIfStringDebian $1 $2)$(getConfigIpStringDebian $3 $4 $5)/" $NETWORK_CONFIG_DEBIAN
 	return 0
 
 }
+
+##################################################
+# Ham cau hinh ip cho may redhat
+#
 setIpRedhat(){
 	[ -f "$NETWORK_CONFIG_REDHAT$1" ] || echo "" > $NETWORK_CONFIG_DEBIAN$1
 	[ -n "$2" ] && { grep -q "^BOOTPROTO=" $NETWORK_CONFIG_DEBIAN$1 && sed "s/^BOOTPROTO=.*/BOOTPROTO=$2/" -i $NETWORK_CONFIG_DEBIAN$1 || sed "$ a\BOOTPROTO=$2" -i $NETWORK_CONFIG_DEBIAN$1; }
@@ -47,35 +95,62 @@ setIpRedhat(){
 	
 	grep -q "^FOOBAR=" file && sed "s/^FOOBAR=.*/FOOBAR=newvalue/" -i file || sed "$ a\FOOBAR=newvalue" -i file
 }
+
+##################################################
+# Ham lay dia chi ip tren may debian
+#
 getIpInfoDebian(){
 	[ $# -eq 0 ] && return 1
+	# lenh sed lay dia chi ip
 	ip=$(sed -n "/iface $1/,/iface/ s/address//p"  $NETWORK_CONFIG_DEBIAN|sed -e 's/^[[:space:]]*//')
+	# lenh sed lay subnetmask
 	netmask=$(sed -n "/iface $1/,/iface/ s/netmask//p"  $NETWORK_CONFIG_DEBIAN|sed -e 's/^[[:space:]]*//')
+	# ghi log file
 	echo "config getIpInfoDebian: IP:$ip" >> $LOG_FILE
 	echo "config getIpInfoDebian: netmask:$netmask" >> $LOG_FILE
+	# kiem tra ip co ton tai trong file cau hinh
 	[ -n "$ip" -a -n "$netmask" ] && { echo "$ip:$netmask"; return 0; }
+	# lay dia chi ip tu lenh ifconfig
 	ipInfo=$(getIfconfigInfo $1)
+	# ghi log file
 	echo "config getIpInfoDebian: ipInfo:$ipInfo" >> $LOG_FILE
 	[ -n "$ipInfo" ] && echo $ipInfo || return 1
 }
+
+##################################################
+# Ham kiem tra phien ban he dieu hanh co phai Debian hay Ubuntu
+#
 isDebianOS(){
 	local VERSION_FILE="/proc/version"
 	version=$(cat $VERSION_FILE)
-	[[ "$version" == *"ubuntu"* ]] || [[ "$version" == *"debian"* ]] && return 0 || return 1
+	[[ "$version" == *"ubuntu"* ]] || [[ "$version" == *"debian"* ]] \
+	&& { echo "config isDebianOS: Debian OS" >> $LOG_FILE; return 0; } \
+	|| { echo "config isDebianOS: Not Debian OS" >> $LOG_FILE; return 1; }
 }
-checkCidr(){
-	(($1>=0 && $1<=32)) && return 0
-	return 1
-}
+
+##################################################
+# Cau hinh IP
+#
 settingIp(){
 	result=$(isDebianOS && setIpDebian $1 $2 $3 $4 $5 || setIpRedhat $1 $2 $3 $4 $5)
+	ifdown eth0 && ifup eth0
 	echo "config settingIp: $result" >> $LOG_FILE; 
 }
+
+##################################################
+# Lay thong tin IP cu
+#
 getInfoIp(){
+	# nau may la Debian hay Ubuntu thi goi ham getIpInfoDebian
+	# nguoc lai goi getIpInfoRedhat
 	isDebianOS && result=$(getIpInfoDebian $1) || result=$(getIpInfoRedhat $1)
 	echo $result
 	echo "config getInfoIp: $result" >> $LOG_FILE; 
 }
+
+##################################################
+# Ham main
+#
 main(){
 		[ "$3" == "static" -o "$3" == "dhcp" ] && settingIp $2 $3 $4 $5 $6 \
 		&& echo  $1:$2:$(getInfoIp $2):$3:$4:$5:$6\
--- setting.sh
+++ setting.sh
@@ -1,18 +1,126 @@
-
-# import
-. cidr2mask.sh
-. mask2cidr.sh
-
+#!/bin/bash
+##################################################
+# Name: setting.sh
+# Description: main setting file
+#
+##################################################
+# Set Script Variables
+#
 DEFAULT_NETMASK="255.255.255.0"
 SCRIP_FILE="configuration.sh"
 SYSADMIN_USERNAME="hiep"
 RESULT_FILE="result.txt"
+LOG_FILE="settingIpLog.txt"
+SSH_ERROR="Can not connect to host"
+
+##################################################
+# Ham kiem tra netmask
+#
+mask2cidr() {
+	nbits=0
+	local IFS=.
+	for dec in $1 ; do
+		case $dec in
+			255) let nbits+=8;;
+			254) let nbits+=7;;
+			252) let nbits+=6;;
+			248) let nbits+=5;;
+			240) let nbits+=4;;
+			224) let nbits+=3;;
+			192) let nbits+=2;;
+			128) let nbits+=1;;
+			0);;
+			*) return 1
+		esac
+	done
+	return 0
+}
+
+##################################################
+# Ham chuyen so sang subnetmask
+#
+cidr2mask() {
+	local full_octets=$(($1/8))
+	local partial_octet=$(($1%8))
+	local i mask=""  
+	for ((i=0;i<4;i+=1)); do
+		if [ $i -lt $full_octets ]; then
+			mask+=255
+		elif [ $i -eq $full_octets ]; then
+			mask+=$((256 - 2**(8-$partial_octet)))
+		else
+			mask+=0
+		fi  
+		test $i -lt 3 && mask+=.
+	done
+	echo $mask
+}
+
+##################################################
+# Ham kiem tra dia chi ip
+#
+isValidip(){
+	case "$*" in
+	""|*[!0-9.]*|*[!0-9]) return 1 ;;
+	esac
+
+	local IFS=.
+	set -- $*
+
+	[ $# -eq 4 ] &&
+	[ ${1:-666} -le 255 ] && [ ${2:-666} -le 255 ] &&
+	[ ${3:-666} -le 255 ] && [ ${4:-666} -le 254 ]
+}
+
+##################################################
+# Ham kiem tra Subnet 
+#
+checkCidr(){
+	case "$*" in
+	""|*[!0-9]*) return 1 ;;
+	esac
+	(("$1">=0 && "$1"<=32)) && return 0
+	return 1
+}
+
+##################################################
+# Gan dia chi ip tu dong neu khong co 
+#
+getAutoIp(){
+	local IFS=.
+	[ $# -eq 4 ] || return 1
+	set -- $*
+	echo $1.$2.$3.$(($4+1))
+}
+
+##################################################
+# Chuong trinh chinh 
+#
 main(){
-	while IFS=: read host iface status setip setnetmask setgateway
+	while IFS=: read thisHost thisIface thisStatus thisSetip thisSetnetmask thisSetgateway
 	do
+		echo "******** Setting ip for host $thisHost:$thisIface **********"
+		echo "read file " $thisHost $thisIface $thisStatus $thisSetip $thisSetnetmask $thisSetgateway
+		# kiem tra dia chi ip hoac hostname
+		[ -n "$thisHost" ] && host=$thisHost
+		# kiem tra interface
+		[ -n "$thisIface" ] && iface=$thisIface
+		# trang thai la static hay dhcp hay null
+		status=$thisStatus
+		# kiem tra dia chi ip
+		[ -n "$thisSetip" ] && isValidip $thisSetip && setip=$thisSetip || setip=$(getAutoIp $setip) 
+		# kiem tra subnetmask
+		[ -n "$thisSetnetmask" ] && setnetmask=$thisSetnetmask
+		# kiem tra gateway
+		[ -n "$thisSetgateway" ] && isValidip $thisSetgateway && setgateway=$thisSetgateway
+		checkCidr $setnetmask && cidr2mask $setnetmask && setnetmask=$(cidr2mask $setnetmask) \
+		|| mask2cidr $setnetmask || setnetmask=$DEFAULT_NETMASK
+		###########
 		echo "setting file " $host $iface $status $setip $setnetmask $setgateway
-		result=$(ssh $SYSADMIN_USERNAME@$host 'bash -s' < $SCRIP_FILE $host $iface $status $setip $setnetmask $setgateway)
-		echo $result >> $RESULT_FILE
+		# chay script cau hinh ip tren remote host
+		ssh $SYSADMIN_USERNAME@$host 'bash -s' < $SCRIP_FILE $host $iface $status $setip $setnetmask $setgateway >> $RESULT_FILE
+		# doc file log va xoa
+		ssh $SYSADMIN_USERNAME@$host "cat $LOG_FILE ; rm $LOG_FILE" >> $LOG_FILE
 	done < "$1"
 }
 
